2962c9b271284ba84c54c749bf536c49e185c097
Firstname Lastname <ssirojiddinov777@gmail.com>
2023-04-18 13:36:04 +0500

    Some changes added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Projects(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    img = models.ImageField(upload_to='media/')
+    link =  models.URLField(null=True, blank=True)
+    exralink = models.URLField(null=True, blank=True)
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    text = models.TextField()
+
+class Services(models.Model):
+    img = models.ImageField(upload_to='media/')
+    name = models.CharField(max_length=200)
+    text = models.TextField(null=True, blank=True)
+
+class About(models.Model):
+    img = models.ImageField(upload_to='media/')
+    inform = models.TextField()
+
+
+
+class Friends(models.Model):
+    img = models.ImageField(upload_to='media/')
+    name = models.CharField(max_length=200)
+    inform = models.TextField()
+
+
+
+class Blog(models.Model):
+    img = models.ImageField(upload_to='media/')
+    name = models.CharField(max_length=200)
+    inform = models.TextField()
+    extra = models.TextField()
--- main/views.py
+++ main/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import *
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    about = About.objects.last
+    project = Projects.objects.all()
+    serv = Services.objects.all()
+    friend = Friends.objects.all()
+    blog = Blog.objects.all()
+    context = {
+        'about': about,
+        'project': project,
+        'friend': friend,
+        'blog': blog,
+        'serv': serv
+    }
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        comment = request.POST['comment']
+        Contact.objects.create(email=email, name=name, text=comment)
+
+    return render(request, 'index.html', context)
+
